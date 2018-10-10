608e384e78664f8cfbc2f6cd7aef47e6286d9cb7
Caio Melo <caiofelipee17@gmail.com>
2018-10-10 01:18:55 -0300

    Add jwt token validator

---
+++ util/token.validator.js
@@ -0,0 +1,15 @@
+let jwt = require('jsonwebtoken');
+
+function verifyTokenEnv(req, res, next) {
+    let token = req.headers.authorization;
+    if (!token) return res.status(400);
+    jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
+        if(err) return res.status(400);
+        req.userId = decoded.id;
+        req.houseId = decoded.house;
+        next();
+    })
+}
+
+module.exports = verifyTokenEnv;
+
